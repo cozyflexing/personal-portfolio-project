824a2095f784491ddc93fcb4657dfa9ac1751906
cozyflexing <cozyflexing@gmail.com>
2022-05-29 23:08:32 +0200

    minor update to blog detail page linking

--- blog/urls.py
+++ blog/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from . import views
 
+app_name = "blog"
+
 urlpatterns = [
     path("", views.blogs_home, name="blogs_home"),
     path("<int:blog_id>", views.detail, name="detail"),
