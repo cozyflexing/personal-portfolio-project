712b41bf5601600af730d6be0630d2c638a6f964
cozyflexing <cozyflexing@gmail.com>
2022-05-29 23:40:36 +0200

    final functionality added

--- blog/models.py
+++ blog/models.py
@@ -7,3 +7,6 @@ class Blog(models.Model):
     title = models.CharField(max_length=100)
     date = models.DateField(auto_now_add=True)
     description = models.CharField(max_length=250)
+
+    def __str__(self):
+        return self.title
