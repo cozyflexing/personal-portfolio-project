2b54327283ab3247c1df8ad6e5fbb35b31f83acb
cozyflexing <cozyflexing@gmail.com>
2022-05-29 23:03:47 +0200

    Added detail pages for each blog

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Blog
 
 # Create your views here.
@@ -9,3 +9,10 @@ def blogs_home(request):
     blogs = Blog.objects.order_by("-date")[:5]
 
     return render(request, "blog/blogs_home.html", {"blogs": blogs})
+
+
+def detail(request, blog_id):
+
+    blog = get_object_or_404(Blog, pk=blog_id)
+
+    return render(request, "blog/detail.html", {"blog": blog})
