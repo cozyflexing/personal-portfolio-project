550e12c4ab36de565e42df29a054b7ef6f12da58
cozyflexing <cozyflexing@gmail.com>
2022-05-30 19:05:00 +0200

    finished basic bootstrap on all pages

--- blog/models.py
+++ blog/models.py
@@ -6,7 +6,7 @@ from django.db import models
 class Blog(models.Model):
     title = models.CharField(max_length=100)
     date = models.DateField(auto_now_add=True)
-    description = models.TextField(max_length=1000)
+    description = models.TextField()
 
     def __str__(self):
         return self.title
