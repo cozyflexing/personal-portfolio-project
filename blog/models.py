4a09eea83df3a78f7d0e13fd98e29913e221f93d
cozyflexing <cozyflexing@gmail.com>
2022-05-30 18:51:06 +0200

    idk anymore

--- blog/models.py
+++ blog/models.py
@@ -6,7 +6,7 @@ from django.db import models
 class Blog(models.Model):
     title = models.CharField(max_length=100)
     date = models.DateField(auto_now_add=True)
-    description = models.CharField(max_length=250)
+    description = models.TextField(max_length=1000)
 
     def __str__(self):
         return self.title
